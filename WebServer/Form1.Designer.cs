191da42da08840c42769b11744ee6f490c22ca28
‘Blake <blake.peters@gmail.com>
2019-09-25 23:19:56 -0500

    Initial Creation

--- WebServer/Form1.Designer.cs
+++ WebServer/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WebServer
 {
-    partial class Form1
+    partial class WebServerPage
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Start = new System.Windows.Forms.Button();
+            this.StopEnd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(61, 41);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(158, 45);
+            this.Start.TabIndex = 0;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // StopEnd
+            // 
+            this.StopEnd.Location = new System.Drawing.Point(61, 140);
+            this.StopEnd.Name = "StopEnd";
+            this.StopEnd.Size = new System.Drawing.Size(158, 45);
+            this.StopEnd.TabIndex = 1;
+            this.StopEnd.Text = "Stop";
+            this.StopEnd.UseVisualStyleBackColor = true;
+            this.StopEnd.Click += new System.EventHandler(this.StopEnd_Click);
+            // 
+            // WebServerPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.StopEnd);
+            this.Controls.Add(this.Start);
+            this.Name = "WebServerPage";
+            this.Text = "WebServer";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Button StopEnd;
     }
 }
 
--- WebServer/Form1.cs
+++ WebServer/Form1.cs
@@ -4,17 +4,56 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WebServer
 {
-    public partial class Form1 : Form
+    public partial class WebServerPage : Form
     {
-        public Form1()
+        private int port = 2858;
+
+        IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+        TcpListener server = null;
+
+
+        public WebServerPage()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //ThreadPool.QueueUserWorkItem(StartServer, port);
+            StartServer(port);
+
+            MessageBox.Show("Hello! How are you?");
+        }
+
+        private void StopEnd_Click(object sender, EventArgs e)
+        {
+            server.Stop();
+            MessageBox.Show("I'm well! Thanks for asking!! :)");
+        }
+        private void StartServer(int port)
+        {
+            server = new TcpListener(localAddr, port);
+            server.Start();
+        }
+        public void SetForegroundData(string value)
+        {
+            // Is this a background thread? If so, call it's parent
+            if (InvokeRequired)
+            {
+                this.BeginInvoke(new Action<string>(SetForegroundData), new object[] { value });
+                return;
+            }
+            // If parent, send the value to the parent
+            //textOutput.Text = value;
         }
     }
 }
